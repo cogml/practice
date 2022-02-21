98f35190c44a2ffa5130a705f7e877b0a547547f
cogml <cogml1212@naver.com>
2022-02-21 23:07:49 +0900

    Flatten과제 제출

--- funFlatten.kt
+++ funFlatten.kt
@@ -6,8 +6,8 @@ fun main() {
     println(result)
 }
 
-fun Flatten(lst: MutableList<Any>) : MutableList<Int> { //재귀함수로 구현
-    var result_lst : MutableList<Int> = mutableListOf()
+fun Flatten(lst: MutableList<Any>) : MutableList<Any> { //재귀함수로 구현
+    var result_lst : MutableList<Any> = mutableListOf()
     var count  = 1
     var size = lst.size
     return if(count <= 0){ //count = 0일 때 까지(count는 정수형이아닌 원소(즉, 배열 형태인 원소)의 개수)
@@ -17,12 +17,13 @@ fun Flatten(lst: MutableList<Any>) : MutableList<Int> { //재귀함수로 구현
         count = 0
         for(i in 0..size-1){
             if(lst[i] is Int){ //lst[i]가 정수라면 
-                
-                result_lst.add(lst[i]) //Int형 결과 리스트에 값을 추가 => 여기서 lst[i]는 정수일 수밖에 없는데 타입 불일치 에러 발생..
+                result_lst.add(lst[i]) 
             }
             else{
                 count++
-                Flatten(lst[i])
+                var sub_lst : MutableList<Any> = mutableListOf()
+                sub_lst.add(lst[i])
+                Flatten(sub_lst)
             }
         count = 0
         }
